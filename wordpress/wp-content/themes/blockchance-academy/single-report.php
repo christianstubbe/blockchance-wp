c6789bdc6d1cc17e4fc76998b9a307fa9d85e1d5
Christian Stubbe <christian.stubbe@omnicommediagroup.com>
2021-04-19 11:42:22 +0200

    registered-post-types

---
+++ wordpress/wp-content/themes/blockchance-academy/single-report.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying definition posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Blockchance_Academy
+ */
+
+get_header();
+?>
+	<main id="primary" class="site-main">
+		<?php while ( have_posts() ) : the_post(); ?>
+        <!---------    START EDITING HERE    ---------> 
+
+            <h1>TEMPLATE REPORT</h1>
+
+        <!---------    STOP EDITING HERE    ---------> 
+		<?php endwhile; ?>
+	</main>
+<?php
+get_sidebar();
+get_footer();
